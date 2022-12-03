033aa477c2d8dfba1e92b98da6f253ad20ea442d
Diego Reis <diegoluis200302@gmail.com>
2022-12-02 21:54:44 -0300

    epsilon processing bug fix

--- src/NFA_Printer.hpp
+++ src/NFA_Printer.hpp
@@ -10,8 +10,6 @@ namespace NFA_Printer
 {
     const char CHAIN_IS_ACCEPTED_SYMBOL = 'V';
     const char CHAIN_IS_NOT_ACCEPTED_SYMBOL = 'X';
-    const std::string CHAIN_IS_ACCEPTED_MSG = "A cadeia processada eh aceita. \n";
-    const std::string CHAIN_IS_NOT_ACCEPTED_MSG = "A cadeia processada nao eh aceita. \n";
 
     void PrintProcessmentTree(NaryTree<Transition*>* processmentTree);
     void PrintProcessmentList(MyList<NaryTree_Node<Transition*>*> node);
